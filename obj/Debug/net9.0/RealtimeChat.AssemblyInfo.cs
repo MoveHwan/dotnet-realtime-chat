7f2e2a0d935a83e9ca87ec1d98f6d60c32599430
MoveHwan <kso702003@gmail.com>
2026-07-14 19:14:18 +0900

    main

--- obj/Debug/net9.0/RealtimeChat.AssemblyInfo.cs
+++ obj/Debug/net9.0/RealtimeChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealtimeChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f42981b1a94b666c6fd131a5e2c7fa2928b7062e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4767a1d1d17ba4d462c45834932931b74fa3008f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealtimeChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealtimeChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
